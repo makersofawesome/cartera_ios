c598a0708f7457de155a2e28782743fb679d6e90
Vincent Le <vinnyoodles@gmail.com>
2016-02-27 00:15:35 -0500

    add gitignore

---
+++ Cartera/Client.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  Cartera
+//
+//  Created by Vincent Le on 2/26/16.
+//  Copyright © 2016 Samee Khan. All rights reserved.
+//
+
+import UIKit
+
+class Client: NSObject {
+
+}
