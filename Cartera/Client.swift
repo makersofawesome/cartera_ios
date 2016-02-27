5a980fe465dd8f0ee6c5bf67ef73a6d7a8864e72
Vincent Le <vinnyoodles@gmail.com>
2016-02-27 01:47:09 -0500

    create client file

---
+++ Cartera/Client.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  Cartera
+//
+//  Created by Vincent Le on 2/27/16.
+//  Copyright © 2016 Samee Khan. All rights reserved.
+//
+
+import UIKit
+
+class Client: NSObject {
+
+}
