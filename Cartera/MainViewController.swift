d2b523ac0687e61587e70a03d67db4b2be9d9ee4
Vincent Le <vinnyoodles@gmail.com>
2016-02-27 11:24:51 -0500

    get user data from server

--- Cartera/MainViewController.swift
+++ Cartera/MainViewController.swift
@@ -7,27 +7,44 @@
 //
 
 import UIKit
+import Parse
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    var users: [User]?
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        var image = UIImage(named: "composeButton")
-//        image = image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
-
-        // Do any additional setup after loading the view.
         self.navigationItem.setHidesBackButton(true, animated:false)
+        tableView.delegate = self
+        tableView.dataSource = self
+        //loadUsers()
     }
-
+    func loadUsers() {
+        var query = PFQuery(className: "_User")
+        query.findObjectsInBackgroundWithBlock { (media: [PFObject]?, error: NSError?) -> Void in
+            if let media = media {
+                self.users = User.usersWithArray(media)
+                self.tableView.reloadData()
+            } else {
+                // handle error
+            }
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if let data = users {
+            return data.count
+        }
+        else {
+            return 0
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -38,7 +55,8 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     @IBAction func onCompose(sender: AnyObject) {
-        Client.sharedClient.createTransfersWithParams("56c66be6a73e492741507e12", params: NSDictionary())
+        Client.sharedClient.createTransferWithParams("56c66be6a73e492741507e0b")
+       // Client.sharedClient.getTransfersFromAccountId("56c66be6a73e492741507e0a")
     }
 
 }
--- Cartera/User.swift
+++ Cartera/User.swift
@@ -7,7 +7,23 @@
 //
 
 import UIKit
+import Parse
 
 class User: NSObject {
 
+    var username: String?
+    var id: String?
+    
+    init(object: PFObject){
+        username = object["username"] as? String
+        id = object["_id"] as? String
+    }
+    class func usersWithArray(array: [PFObject]) -> [User] {
+        var users = [User]()
+        
+        for obj in array {
+            users.append(User(object: obj))
+        }
+        return users
+    }
 }
