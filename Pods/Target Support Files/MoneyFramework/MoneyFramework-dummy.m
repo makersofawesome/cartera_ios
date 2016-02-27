554ba40e72323115de12be4461bb11b05f58356e
Samee Khan <samee@vt.edu>
2016-02-27 13:09:46 -0500

    compose vc

---
+++ Pods/Target Support Files/MoneyFramework/MoneyFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MoneyFramework : NSObject
+@end
+@implementation PodsDummy_MoneyFramework
+@end
---
+++ Pods/Target Support Files/MoneyFramework/MoneyFramework-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MoneyFrameworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char MoneyFrameworkVersionString[];
+
