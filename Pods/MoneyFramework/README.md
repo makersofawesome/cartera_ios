7992a9bfd19ebcd207d9d0da9c134088e6ffcb00
Vincent Le <vinnyoodles@gmail.com>
2016-03-19 08:41:34 -0400

    remove pods and binaries from dev

--- Pods/MoneyFramework/README.md
+++
@@ -1,2 +0,0 @@
-# Money Design Pattern in Swift
-Simple implementation of The Money Pattern, defined by Martin Fowler.
--- Pods/Target Support Files/MoneyFramework/MoneyFramework-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double MoneyFrameworkVersionNumber;
-FOUNDATION_EXPORT const unsigned char MoneyFrameworkVersionString[];
-
